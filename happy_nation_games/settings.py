b95eb2b430c5a52c9d4eab6751f7d3951faf0a50
User <gasimov.r.rg@gmail.com>
2023-12-02 14:07:57 +0100

    adding cloudbuild.yaml file

--- happy_nation_games/settings.py
+++ happy_nation_games/settings.py
@@ -11,7 +11,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-y0goccaa0oss$gslh#w1-suxqk+i(y&d^k@gty60_a7p1(*b5j'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
