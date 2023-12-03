f735d56c37a4dbd607af7f49293eb1821d4afa6f
User <gasimov.r.rg@gmail.com>
2023-12-03 15:15:53 +0100

    modify docker

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,8 @@
-# Set Base Image
-FROM python:3.9
-# set environment variables
-ENV PYTHONUNBUFFERED=1
-# set work directory
+FROM python:3.10
+ENV PYTHONUNBUFFERED 1
+ENV DEBUG False
+COPY requirements.txt /code/requirements.txt
 WORKDIR /code
-# install dependencies
-COPY requirements.txt /code/
 RUN pip install -r requirements.txt
-# copy project
-COPY . /code/
+ADD . .
 CMD [ "gunicorn", "--bind", "0.0.0.0", "-p", "8000",  "happy_nation_games.wsgi" ]
--- happy_nation_games/settings.py
+++ happy_nation_games/settings.py
@@ -43,7 +43,7 @@ ROOT_URLCONF = 'happy_nation_games.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -105,7 +105,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 if DEBUG:
     STATICFILES_DIRS = [
         os.path.join(BASE_DIR, "static")
